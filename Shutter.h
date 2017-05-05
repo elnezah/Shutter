405f23447d53a404298cc9fe38d1cb9d20d76970
elnezah <elnezah@gmail.com>
2017-05-05 17:53:11 +0200

    Update Shutter.h

--- Shutter.h
+++ Shutter.h
@@ -1,29 +1,37 @@
+#ifndef SHUTTER_H
+#define SHUTTER_H
+#include "Servo.h"
+
 /*
  * Shutter.h - Library to make a photocamera shutter out of a servo
  *   alternatively it could press any physical button with a servo.
  */
 
-
-#ifndef Shutter
-#define Shutter
-
-#include "Arduino.h"
-#include "Servo.h"
-
 class Shutter {
   public:
-    Shutter(Servo servo);
+    Shutter(Servo *servo);
     void setPressedPosition(int pos);
     void setReleasedPosition(int pos);
     void setPressTime(int pTime);
-    void shut();
-    Servo getServo();
+    void setRepetitions(int reps);
+    void setWaitTime(int waitTime);
+    
+    int getPressedPosition();
+    int getReleasedPosition();
+    int getPressTime();
+    int getRepetitions();
+    int getWaitTime();
+    Servo *getServo() const;
+    
+    void shutOnce();
+    void doBurst();
   private:
-    Servo _servo;
+    Servo *_servo;
     int _pressedPosition;
     int _releasedPosition;
     int _pressTime;
-    
-}
+    int _repetitions;
+    int _waitTime;   
+};
 
 #endif
